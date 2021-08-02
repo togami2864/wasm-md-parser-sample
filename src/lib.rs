c8af249c2468991ef606fc01573f5967134e26c1
Yoshiaki Togami <tuabobo123@gmail.com>
2021-08-02 21:46:54 +0900

    bind wasm_bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,9 @@ mod parser;
 
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn parse(input: &str) -> String {
+    let result = parser::parse(input.to_string());
+    result
+}
