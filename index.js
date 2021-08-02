c809d868ab463199bbab278bf15cf5d8406321e5
Yoshiaki Togami <tuabobo123@gmail.com>
2021-08-03 00:03:31 +0900

    enable to output in realtime

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const rust = import("./markdown_parser");
 
 rust.then((module) => {
-  const btn = document.getElementById("parse");
   const previewArea = document.getElementById("output");
-
-  btn.addEventListener("click", () => {
-    const input = document.getElementById("markdown").value;
-    previewArea.innerHTML = module.parse(input);
+  const input = document.getElementById("markdown");
+  input.addEventListener("input", () => {
+    text = input.value;
+    previewArea.innerHTML = module.parse(text);
   });
 });
